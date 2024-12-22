e2629e01a8e60c9f2ad34015d793e1315fa9db50
M Tayyab Saleem <tayyabsknowledge985@gmail.com>
2024-12-22 16:55:35 +0500

    All Files Added

--- backend/app.js
+++ backend/app.js
@@ -61,7 +61,7 @@ const sessionOptions = {
    expires: Date.now() + 7*24*60*60*1000,
    maxAge:  7*24*60*60*1000,
    httpOnly: true,
-   secure: false, 
+   secure: true, 
    sameSite: 'None'
   }
 };
